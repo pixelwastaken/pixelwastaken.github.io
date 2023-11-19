58c632b6974cd768c18e1b271b34e4b65ef18e4c
pixelwastaken <93735955+pixelwastaken@users.noreply.github.com>
2023-11-19 09:22:09 -0500

    changed main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const buttons = document.getElementsByClassName("play-game-b");
 alert(buttons);
+alert("what");
 for (let e of buttons) {
     e.onclick = function () {
         alert(e.innerHTML);
