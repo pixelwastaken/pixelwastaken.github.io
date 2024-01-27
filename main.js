246a49e4b71752641d091dcd4e3d0d5188e8c1ba
pixelwastaken <93735955+pixelwastaken@users.noreply.github.com>
2024-01-27 14:23:42 -0500

    added load system

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-const buttons = document.querySelectorAll(".play-game-b");
-alert(buttons);
-alert(buttons[0]);
-for (let e of buttons) {
-    alert(e)
-    
-    e.onclick = function () {
-        alert(e.innerHTML);
-    };
-}
+
+window.onload = function () {
+    const buttons = document.querySelectorAll(".play-game-b");
+    alert(buttons);
+    alert(buttons[0]);
+    for (let e of buttons) {
+        alert(e)
+        
+        e.onclick = function () {
+            alert(e.innerHTML);
+        };
+    }
+};
