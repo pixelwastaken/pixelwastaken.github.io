8e8811a156738b66afbc39fb818e3b170c464756
pixelwastaken <93735955+pixelwastaken@users.noreply.github.com>
2024-06-08 22:51:30 -0400

    removed debugs

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ window.onload = function () {
     const buttons = document.querySelectorAll(".play-game-b");
 
     for (let e of buttons) {
-        alert(e)
 
         let gameid = e.parentElement.id
         
